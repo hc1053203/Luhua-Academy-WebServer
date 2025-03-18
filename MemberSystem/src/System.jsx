e21ed245a8e3926616217b314b07c272a8323909
幻影 <hc1053203@gmail.com>
2025-03-18 13:49:34 +0800

    更新會員系統

--- MemberSystem/src/System.jsx
+++ MemberSystem/src/System.jsx
@@ -3,20 +3,33 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./style.css";
 
+
 function System() {
-  return (
+  const [username,setusername]=useState('')
+  const [password,setpassword]=useState('')
+  return(
     <div>
       <h1>會員登入</h1>
       <div className="container login">
         <p>帳號:</p>
-        <input type="text" className="login"></input>
+        <input type="text" className="login"
+        value={username}
+        onChange={(e)=>{setusername(e.target.value)}}
+        />
       </div>
       <div className="container login">
         <p>密碼:</p>
-        <input type="password" className="login"></input>
+        <input type="password" className="login"
+        value={password}
+        onChange={(e)=>{setpassword(e.target.value)}}
+        />
       </div>
     </div>
-  );
+  )
+}
+
+function Login(){
+  
 }
 
 export default System;
